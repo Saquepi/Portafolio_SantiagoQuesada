69006080b73c64586d4975862e3f46666703af55
Saquepi <145624844+Saquepi@users.noreply.github.com>
2024-10-22 23:23:21 -0600

    semana 5
    implementar codigo y archivos de la semana 5

---
+++ TiendaSantiagoQuesada/src/main/java/tienda/demo/dao/CategoriaDao.java
@@ -0,0 +1,9 @@
+
+package tienda.demo.dao;
+
+import tienda.demo.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository <Categoria,Long> {
+    
+}
---
+++ TiendaSantiagoQuesada/src/main/java/tienda/demo/domain/Categoria.java
@@ -0,0 +1,35 @@
+package tienda.demo.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+    public Long idCategoria;
+    public String descripcion;
+    public String rutaImagen;
+    public boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String categoria, boolean activo) {
+        this.descripcion = categoria;
+        this.activo = activo;
+    }
+    
+    public boolean isActivo(){
+        return activo;
+    }
+    
+    
+}
